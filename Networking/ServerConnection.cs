8bc4e2e551b5a224c839f6b1d747d26756d4f8fa
bnelson-mtb <bradyscottnelson@gmail.com>
2025-11-05 20:14:48 -0700

    Implemented WaitForConnections

--- Networking/ServerConnection.cs
+++ Networking/ServerConnection.cs
@@ -26,7 +26,40 @@ public static class ServerConnection
     /// <param name="port"> The port (e.g., 11000) to listen on. </param>
     public static void WaitForConnections( Action<NetworkConnection> handleConnect, int port, ILogger logger )
     {
-        // TODO: Implement this - should look very much like sample code from class.
-        throw new NotImplementedException();
+        TcpListener s = new TcpListener( IPAddress.Any, port );
+        s.Start();
+        logger.LogInformation( "Server listening on port {0}", port );
+        while (true)
+        {
+            TcpClient client = s.AcceptTcpClient();
+            logger.LogInformation( "Client connected." );
+
+            NetworkConnection connection = new( client, logger );
+
+            if ( !connection.IsConnected )
+            {
+                logger.LogWarning( "Connection failed to establish." );
+                continue;
+            }
+
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    handleConnect( connection );
+                }
+                catch ( Exception e )
+                {
+                    logger.LogError( "Error in connection handler: {0}", e );
+                }
+                finally
+                {
+                    connection.Disconnect();
+                    logger.LogInformation( "Client disconnect." );
+                }
+            } );
+
+            thread.Start();
+        }
     }
 }
